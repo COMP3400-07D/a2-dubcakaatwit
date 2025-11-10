3ecb116095f8e585bfc32327d66b2539a78f85fe
alemdubcak <alemdubcak@alem.os.com>
2025-11-10 14:58:02 -0500

    Completed others

--- string_fill.c
+++ string_fill.c
@@ -4,4 +4,13 @@
  * TODO: Describe what the function does
  */
 void string_fill(char dest[], int dest_cap, char c) {
+    if (dest == NULL || dest_cap <= 0)
+        return;
+
+    int i;
+    for (i = 0; i < dest_cap - 1; i++) {
+        dest[i] = c;
+    }
+
+    dest[i] = '\0';
 }
